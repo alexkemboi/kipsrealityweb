ca503d9b48348642d4c53ae283789771ab0ec3f7
HassanMunene <awanzihassan@gmail.com>
2025-10-19 17:13:20 +0300

    removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeaturesSection from "./components/features-section";
