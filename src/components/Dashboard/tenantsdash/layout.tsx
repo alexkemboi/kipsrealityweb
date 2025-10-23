429725acdfb6e6eaa76e408b55692d2b2875b3a9
ALEX KEMBOI <AKEMBOI@fintech-group.com>
2025-10-23 04:12:05 +0300

    fixed tenantsdash

--- src/components/Dashboard/tenantsdash/layout.tsx
+++ src/components/Dashboard/tenantsdash/layout.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <div className='h-screen'>
             <Navbar />
             <div className='w-full xs:flex-col md:w-full md:flex md:justify-between'>
-                <div className='w-1/5 md:block bg-neutral-900/95'>
+                <div className='w-1/5 md:block bg-[#0f172a]'>
                     <SideBarComponent />
                 </div>
                 <div className='overflow-y-auto h-full md:align-center md:justify-center w-4/5 m-auto'>
