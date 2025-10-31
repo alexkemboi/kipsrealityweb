e863802fc9b9946fb3be50672a303c72c7d346f9
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2025-10-31 22:38:38 +0300

    feat:testimonial light mode,color adjustment for other sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
-import Image from "next/image";
 import Navbar from "../components/website/Navbar";
 import HeroSection from "../components/website/landing/HeroSection";
 import About from "../components/website/landing/AboutUs";
 import Footer from "../components/website/Footer";
 import WhatMakes from "../components/website/landing/WhatMakes";
 import OurTeam from "../components/website/landing/OurTeam";
-import BrandPromise from "../components/website/landing/BrandPromise";
 import Services from "../components/website/landing/ServicesSection";
-import Plans from "../components/website/landing/Plans";
 import Contact from "../components/website/landing/ContactUs";
 import { Testimonials } from "../components/website/Testimonial/TestimonialClient";
-import ServicePage from "@/app/(website)/services/page"
 import { TypewriterEffectDemo } from "@/components/website/landing/BrandWords";
 import { fetchTestimonials } from "@/lib/testimonial"
 import { fetchAboutUs } from "@/lib/aboutUs";
-
-import { CTASection } from "@/components/website/services/CTASection";
-import { CategorySection } from "@/components/website/services/CategorySection";
-import { QuickStats } from "@/components/website/services/QuickStats";
-import { servicesData } from "./data/servicesData";
+import Gradient from "@/components/website/landing/Gradient";
 import PlansPage from "./(website)/plans/page";
 export const dynamic = "force-dynamic";
 
