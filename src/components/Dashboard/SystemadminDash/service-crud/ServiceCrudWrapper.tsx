3f0182aba19c292adb4b2c9eec0adb323c198a73
nicanornicolas <nicanornicolas4@mylife.mku.ac.ke>
2026-01-19 13:03:52 +0300

    fix(ServiceCrudWrapper): align Prisma queries and field names with schema
    Update `prisma.category.findMany` to `prisma.categories.findMany` and change `service.categoryId` property mapping to `service.category_id` to match the updated database schema and ensure consistent data fetching.

--- src/components/Dashboard/SystemadminDash/service-crud/ServiceCrudWrapper.tsx
+++ src/components/Dashboard/SystemadminDash/service-crud/ServiceCrudWrapper.tsx
@@ -4,7 +4,7 @@ import ServiceCrud from "./ServiceCrud";
 import { Category, Service } from "../../type";
 
 export default async function ServiceCrudWrapper() {
-  const categoriesFromDB = await prisma.category.findMany({
+  const categoriesFromDB = await prisma.categories.findMany({
     include: { services: true },
     orderBy: { id: "asc" },
   });
@@ -17,7 +17,7 @@ export default async function ServiceCrudWrapper() {
     services: cat.services.map((service: any) => ({
       id: service.id,
       name: service.name,
-      categoryId: service.categoryId,
+      categoryId: service.category_id,
       description: service.description,
       impact: service.impact,
       icon: service.icon,
